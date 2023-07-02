d650a9275f4a588df65e00504646888a4c940bb3
Daniel Kononchuk <kononchuk.daniil@knu.ua>
2023-07-02 17:19:48 +0300

    b

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { Main } from './pages/Main';
 import { Shop } from './pages/Shop';
